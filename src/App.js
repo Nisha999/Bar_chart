1d3906c7cbbd6af4c2f861ecc8989e56c747df5c
Nisha999 <61141604+Nisha999@users.noreply.github.com>
2020-03-31 10:38:36 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import { render } from 'react-dom';
+import { scaleBand, scaleLinear } from 'd3-scale';
+import { transition } from 'd3-transition';
+import XYAxis from './axis/xy-axis.js';
+import Grid from './grid/grid.js';
+import Bar from './bar/bar.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:8080/myApp/myApp/subscriptions')
+    .then(response=>{   
+      function groupBy(objectArray, property) {
+        return objectArray.reduce(function (acc, obj) {
+          let key = obj[property]
+          if (!acc[key]) {
+            acc[key] = []
+          }
+          acc[key].push(obj)
+          return acc
+        }, {})
+      }
+      const finalPost=response.data.map(finalVar=>{
+       let groupedPeople = groupBy(response.data, 'preferredStyle')
+       return{
+            category:finalVar.preferredStyle,
+            totalCount:groupedPeople[finalVar.preferredStyle].length
+            } 
+          }
+        )
+      this.setState({data: finalPost});    
+      console.log(finalPost)      
+    });
+  }     
+  render() {
+    const  {data}  = this.state;
+    const parentWidth = 700;
+    const margin = {
+      top: 50,
+      right: 10,
+      bottom: 20,
+      left: 40,
+    };
+    const ticks = 6;
+    const t = transition().duration(1000);
+    const width = parentWidth - margin.left - margin.right;
+    const height = parentWidth * 0.5 - margin.top - margin.bottom;
+
+    const xScale = scaleBand()
+      .domain(data.map(d => (d.category)))
+      .range([0, width])
+      .padding(0.45);
+
+    const yScale = scaleLinear()
+      .domain([0, Math.max(...data.map(d => (d.totalCount)))])
+      .range([height, 0])
+      .nice();
+
+    return (
+      <div className='App-chart-container'>
+        <svg width={width + margin.left + margin.right} height={height + margin.top + margin.bottom}>
+          <g transform={`translate(${margin.left}, ${margin.top})`}>
+            <XYAxis {...{ xScale, yScale, height, ticks, t }} />
+            <Grid {...{ xScale, yScale, width, ticks, t }} />
+            <Bar
+              {...({
+                xScale,
+                yScale,
+                data,
+                height,
+                t,
+              })}
+            />
+          </g>
+        </svg>
+      </div>
+    );  
+  }
 }
 
 export default App;
---
+++ src/bar/bar.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { select, event } from 'd3-selection';
+import { transition } from 'd3-transition';
+import * as d3 from 'd3'
+const color = ['#f05440', '#d5433d', '#b33535', '#283250'];
+class Bar extends React.Component {
+  constructor() {
+    super();
+    this.ref = React.createRef();
+    
+  }
+  componentDidUpdate() {
+    this.init();
+    this.barTransition();
+  }
+
+  barTransition() {
+    const node = this.ref.current;
+    const { yScale, height, data, t } = this.props;
+
+    select(node)
+      .selectAll('.bar')
+      .data(data)
+      .transition(t)
+      .attr('y', d => yScale(d.totalCount))
+      .attr('height', d => height - yScale(d.totalCount))
+  }
+  init() {
+    
+    const {xScale, yScale, data, height,t} = this.props;
+    const node = this.ref.current;
+
+    // prepare initial data from where transition starts.
+    const initialData = data.map(obj => ({
+      category: obj.category,
+      totalCount: 0
+    }));
+
+ 
+
+    // prepare the field
+    const bar = select(node)
+      .selectAll('.bar')
+      .data(initialData);
+
+      
+      // console.log(initialData);
+    // add rect to svg
+    bar
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', d => xScale(d.category))
+      .attr('y', height)
+      .attr('width', xScale.bandwidth())
+      .style('fill', function(d, i) {
+        return color[i % 4] // use colors in sequence
+      })
+
+    this.barTransition();
+        
+    // bar-label
+    select(node)
+    
+      .selectAll('.bar-label')
+      .data(data)
+      .enter()
+     
+      .append('text')
+      .classed('bar-label', true)
+      .transition(t)
+      .attr('x', d => xScale(d.category) + xScale.bandwidth()/2)
+      .attr('dx', 0)
+      .attr('y', d => yScale(d.totalCount))
+      .attr('dy', -6)
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "10px")
+      .attr("fill", "black")
+      .attr("text-anchor", " middle") 
+      .text(function(d) { return d.totalCount; })   
+
+}
+  render() {
+    return (
+      <g
+        className="bar-group"
+        ref={this.ref}
+      />
+    );
+  }
+}
+
+export default Bar;
